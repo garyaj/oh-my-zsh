891bae5fdb399d0ca210d557440c896c29c88cb0
Gary Ashton-Jones <gary@ashton-jones.com.au>
2012-08-01 12:52:30 +1000

    My customisations

--- modules/utility/init.zsh
+++ modules/utility/init.zsh
@@ -54,41 +54,41 @@ alias rm="${aliases[rm]:-rm} -i"
 alias type='type -a'
 
 # ls
-if is-callable 'dircolors'; then
-  # GNU Core Utilities
-  alias ls='ls --group-directories-first'
-
-  if zstyle -t ':omz:module:utility:ls' color; then
-    if [[ -s "$HOME/.dir_colors" ]]; then
-      eval "$(dircolors "$HOME/.dir_colors")"
-    else
-      eval "$(dircolors)"
-    fi
-    alias ls="$aliases[ls] --color=auto"
-  else
-    alias ls="$aliases[ls] -F"
-  fi
-else
-  # BSD Core Utilities
-  if zstyle -t ':omz:module:utility:ls' color; then
-    export LSCOLORS="exfxcxdxbxegedabagacad"
-    alias ls="ls -G"
-  else
-    alias ls='ls -F'
-  fi
-fi
-
-alias l='ls -1A'         # Lists in one column, hidden files.
-alias ll='ls -lh'        # Lists human readable sizes.
-alias lr='ll -R'         # Lists human readable sizes, recursively.
-alias la='ll -A'         # Lists human readable sizes, hidden files.
-alias lm='la | "$PAGER"' # Lists human readable sizes, hidden files through pager.
-alias lx='ll -XB'        # Lists sorted by extension (GNU only).
-alias lk='ll -Sr'        # Lists sorted by size, largest last.
-alias lt='ll -tr'        # Lists sorted by date, most recent last.
-alias lc='lt -c'         # Lists sorted by date, most recent last, shows change time.
-alias lu='lt -u'         # Lists sorted by date, most recent last, shows access time.
-alias sl='ls'            # I often screw this up.
+# if is-callable 'dircolors'; then
+#   # GNU Core Utilities
+#   alias ls='ls --group-directories-first'
+# 
+#   if zstyle -t ':omz:module:utility:ls' color; then
+#     if [[ -s "$HOME/.dir_colors" ]]; then
+#       eval "$(dircolors "$HOME/.dir_colors")"
+#     else
+#       eval "$(dircolors)"
+#     fi
+#     alias ls="$aliases[ls] --color=auto"
+#   else
+#     alias ls="$aliases[ls] -F"
+#   fi
+# else
+#   # BSD Core Utilities
+#   if zstyle -t ':omz:module:utility:ls' color; then
+#     export LSCOLORS="exfxcxdxbxegedabagacad"
+#     alias ls="ls -G"
+#   else
+#     alias ls='ls -F'
+#   fi
+# fi
+# 
+# alias l='ls -1A'         # Lists in one column, hidden files.
+# alias ll='ls -lh'        # Lists human readable sizes.
+# alias lr='ll -R'         # Lists human readable sizes, recursively.
+# alias la='ll -A'         # Lists human readable sizes, hidden files.
+# alias lm='la | "$PAGER"' # Lists human readable sizes, hidden files through pager.
+# alias lx='ll -XB'        # Lists sorted by extension (GNU only).
+# alias lk='ll -Sr'        # Lists sorted by size, largest last.
+# alias lt='ll -tr'        # Lists sorted by date, most recent last.
+# alias lc='lt -c'         # Lists sorted by date, most recent last, shows change time.
+# alias lu='lt -u'         # Lists sorted by date, most recent last, shows access time.
+# alias sl='ls'            # I often screw this up.
 
 # Mac OS X Everywhere
 if [[ "$OSTYPE" == darwin* ]]; then
